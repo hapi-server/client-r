1d64a8202eb12258fdf4788a1df235f77845c42e
Bob Weigel <rweigel@gmu.edu>
2021-08-05 09:00:49 -0400

    Use size element in info response

--- hapi.r
+++ hapi.r
@@ -1,38 +1,57 @@
 library(RJSONIO)
 library("stringr")
-#library("rjson")
 
 hapi <- function(...) {
   args = list(...)
   if (length(args) == 0) {
     url = "https://github.com/hapi-server/servers/raw/master/all.txt"
-    print(url)
+    print(paste("hapi(): Downloading", url, sep=" "))
     servers = read.csv(url, header=FALSE)
     return(servers["V1"][[1]])
   }
   if (length(args) == 1) {
     url = paste(args[1], "catalog", sep="")
-    print(url)
+    print(paste("hapi(): Downloading", url, sep=" "))
     catalog = fromJSON(url)
     return(catalog)
   }
   if (length(args) == 2) {
     url = paste(args[1], "info?id=", args[2], sep="")
-    print(url)
+    print(paste("hapi(): Downloading", url, sep=" "))
+    info = fromJSON(url)
+    return(info)
+  }
+  if (length(args) == 3) {
+    url = paste(args[1], "info?id=", args[2], "&parameters=", args[3], sep="")
+    print(paste("hapi(): Downloading", url, sep=" "))
     info = fromJSON(url)
     return(info)
   }
   if (length(args) == 5) {
+    
+    # TODO: Why [[]] needed here?
+    meta = hapi(args[[1]], args[[2]], args[[3]])
+
     url = paste(args[1], "data?id=", args[2], "&parameters=", args[3], "&time.min=", args[4], "&time.max=", args[5], sep="")
-    print(url)
+    print(paste("hapi(): Downloading", url, sep=" "))
     csv = read.csv(url, header=FALSE)
     
     parameters = unlist(strsplit(paste("Time", args[3], sep=","), ","))
-
+    
     # Put each column from csv into individual list element
     data = list(csv[, 1])
+    
+    k = 2
     for (i in 2:length(parameters)) {
-      data <- c(data, list(csv[, i]))
+      if ("size" %in% names(meta["parameters"][[1]][[i]])) {
+        size = meta["parameters"][[1]][[i]][['size']]
+      } else {
+        size = 1
+      }
+      size = prod(as.integer(size))
+      print(paste("Extracting columns ", size, "-", (k+size)), sep="")
+      data <- c(data, list(csv[, k:(k+size-1)]))
+      k = k + size
     }    
     # Add names based on request parameters
     # If args[3] = "param1,param2", the following is equivalent to 
@@ -42,28 +61,4 @@ hapi <- function(...) {
     return(data)
   }
 }
-
-servers = hapi()
-print(servers)
-
-catalog = hapi("http://hapi-server.org/servers/TestData2.0/hapi/")
-#catalog = hapi("http://hapi-server.org/servers/SSCWeb/hapi/")
-
-for (i in 1:length(catalog[[1]])) {
-  print(catalog[[1]][[i]]["id"][[1]])
-}
-
-info = hapi("http://hapi-server.org/servers/TestData2.0/hapi/", "dataset1")
-for (i in 1:length(info["parameters"][[1]])) {
-  print(info["parameters"][[1]][[i]][["name"]])
-}
-
-server     = "http://hapi-server.org/servers/TestData2.0/hapi/"
-dataset    = "dataset1"
-parameters = "scalar,scalarstr"
-start      = "1970-01-01Z"
-stop       = "1970-01-01T00:00:11Z"
-
-data = hapi(server, dataset, parameters, start, stop)
-data
 
---
+++ hapi_demo.r
@@ -0,0 +1,37 @@
+# TODO: Make this a relative path.
+source("/Users/weigel/git/hapi/client-r/hapi.r")
+
+if (FALSE) {
+  servers = hapi()
+  print(servers)
+  
+  catalog = hapi("http://hapi-server.org/servers/TestData2.0/hapi/")
+  #catalog = hapi("http://hapi-server.org/servers/SSCWeb/hapi/")
+  
+  for (i in 1:length(catalog[[1]])) {
+    print(catalog[[1]][[i]]["id"][[1]])
+  }
+  
+  info = hapi("http://hapi-server.org/servers/TestData2.0/hapi/", "dataset1")
+  for (i in 1:length(info["parameters"][[1]])) {
+    print(info["parameters"][[1]][[i]][["name"]])
+  }
+
+  server     = "http://hapi-server.org/servers/TestData2.0/hapi/"
+  dataset    = "dataset1"
+  parameters = "scalar,scalarstr"
+  start      = "1970-01-01Z"
+  stop       = "1970-01-01T00:00:11Z"
+  
+  data = hapi(server, dataset, parameters, start, stop)
+  data
+}
+
+server     = "http://hapi-server.org/servers/TestData2.0/hapi/"
+dataset    = "dataset1"
+parameters = "scalar,vector,transformmulti"
+start      = "1970-01-01Z"
+stop       = "1970-01-01T00:00:11Z"
+
+data = hapi(server, dataset, parameters, start, stop)
+data
