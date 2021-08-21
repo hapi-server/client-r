205396ade597a843e0faa4e31438fae508e78ab7
danielwilborn <88450424+danielwilborn@users.noreply.github.com>
2021-08-21 04:44:55 -0700

    Update hapi.r (#3)

--- hapi.r
+++ hapi.r
@@ -1,10 +1,7 @@
 library(RJSONIO)
 library("stringr")
 library(data.table)
-library(curl)
-
 hapi <- function(server = NULL, dataset = NULL, parameters = NULL, start = NULL, stop = NULL) {
-
   if (is.null(server)) {
     url <- "https://github.com/hapi-server/servers/raw/master/all.txt"
     print(paste("hapi(): Downloading", url, sep=" "))
@@ -40,13 +37,13 @@ hapi <- function(server = NULL, dataset = NULL, parameters = NULL, start = NULL,
   csv <- data.table::fread(url)
     
   parameters <- unlist(strsplit(paste("Time", parameters, sep=","), ","))
-
-  # Put each column from csv into individual list element
+    
+    # Put each column from csv into individual list element
   data <- list(csv[, 1])
-
-  # Number of rows (time values)
+    
+    # Number of rows (time values)
   Nr <- nrow(data[[1]])
-
+    
   k = 2
   for (i in 2:length(parameters)) {
     if ("size" %in% names(meta$parameters[[i]])) {
@@ -54,33 +51,54 @@ hapi <- function(server = NULL, dataset = NULL, parameters = NULL, start = NULL,
     } else {
       size <- 1
     }
-
     # Number of columns of parameter
     Nc <- prod(size)
-      
-    print(paste("Extracting columns ", k, "through", (k+Nc-1)), sep="")
-
-    size <- append(Nr, size)
-
     print(paste("Extracting columns ", k, "through", (k+Nc-1)), sep="")
         
-    # Extract columns and re-shape 
+    #convert into a matrix in order to convert into an array 
     data2 <- data.matrix(as.factor(unlist((csv[, k:(k+Nc-1)]))))
-    dim(data2) <- size
-
-    # Add to named list 
-    data <- c(data, list(data2))
+    dim(data2) <- c(Nr, Nc)
+    listfinal <- c()
+    for(p in 1:Nr){
+      if(length(size) == 1){
+        size <- append(1,size)
+      }
+      currentcol <- 1
+      ncolumns <- size[1] * size[2]
+      nmat <- Nc/ncolumns
+      listmat <- list()
+      for(m in 1:nmat){
+        dataformat <- data2[p,currentcol:(currentcol + ncolumns - 1)]
+        dim(dataformat) <- c(size[2],size[1])
+        dataformat <- t(dataformat)
+        listmat[[m]] <- dataformat
+        currentcol <- currentcol + ncolumns
+      }
+      matvec <- c()
+      for(l in 1:nmat){
+        matvec <- append(matvec, listmat[[l]])
+      }
+      array_cur <- array(matvec, dim = size)
+      listfinal[[p]] <- array_cur
+    }
+    finalvec <- c()
+    for(val in 1:Nr){
+      finalvec <- append(finalvec, listfinal[[p]])
+    }
+    size <- append(size, Nr)
+    data_final <- array(finalvec, dim = size)
+    
+ 
+    data <- c(data, list(data_final))
       
     k <- k + Nc
       
   }
-
-  # Add names based on request parameters
-  # If args[3] = "param1,param2", the following is equivalent to 
-  # e.g., names(data) <- c("Time", "param1", "param2")
+    # Add names based on request parameters
+    # If args[3] = "param1,param2", the following is equivalent to 
+    # e.g., names(data) <- c("Time", "param1", "param2")
   names(data) <- c(parameters)
-
+    
   return(data)
-
+  
 }
-
